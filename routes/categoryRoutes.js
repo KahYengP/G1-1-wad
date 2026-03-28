5042f842b696c8f2e89acad966edb0f6c00ff67c
cxs-s <xinhee.sim.2025@smu.edu.sg>
2026-03-28 16:09:27 +0800

    modified category

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,18 +3,19 @@
 const express = require("express");
 const router = express.Router();
 const categoryController = require("../controllers/categoryController");
+const authMiddleware = require('../middleware/authMiddleware');
 
 // Define a GET route to display the list of category
-router.get("/category", categoryController.showCategory);
+router.get("/category", authMiddleware.isAuthenticated, categoryController.showCategory);
 
 // Define GET/POST route to display add form and submit form
-router.get("/add-category", categoryController.showAddForm);
-router.post("/add-category", categoryController.createCategory);
+router.get("/add-category", authMiddleware.isAuthenticated, authMiddleware.isAdmin, categoryController.showAddForm);
+router.post("/add-category", authMiddleware.isAuthenticated, authMiddleware.isAdmin, categoryController.createCategory);
 
 // Define GET/POST route to display all category/edit one category and update result
-router.get("/edit-category", categoryController.showEditForm);
-router.get("/update-category", categoryController.getCategory);
-router.post("/update-category", categoryController.updateCategory);
-router.post("/delete-category", categoryController.deleteCategory);
+router.get("/edit-category", authMiddleware.isAuthenticated, authMiddleware.isAdmin, categoryController.showEditForm);
+router.get("/update-category", authMiddleware.isAuthenticated, authMiddleware.isAdmin, categoryController.getCategory);
+router.post("/update-category", authMiddleware.isAuthenticated, authMiddleware.isAdmin, categoryController.updateCategory);
+router.post("/delete-category", authMiddleware.isAuthenticated, authMiddleware.isAdmin, categoryController.deleteCategory);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -38,6 +38,7 @@ server.set("view engine", "ejs");
 server.use("/", authRoutes);
 server.use("/recipe", recipeRoutes);
 server.use("/", bookmarkRoutes);
+server.use("/", categoryRoutes);
 
 // ========== DATABASE + START ==========
 async function connectDataBase() {
