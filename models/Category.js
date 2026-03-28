22b737b621c1681d448084bf1f032e1885d7611f
cxs-s <xinhee.sim.2025@smu.edu.sg>
2026-03-28 23:11:55 +0800

    modified category

--- models/Category.js
+++ models/Category.js
@@ -1,35 +1,32 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-    categoryName:{
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        minlength: [2, "Category name must be at least 2 characters"],
-        maxlength: [10, "Category name cannot exceed 10 characters"]
-    }
+  categoryName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 10
+  }
 });
 
-categorySchema.statics.getAll = function() {
-  return this.find();
-};
-
-categorySchema.statics.addCategory = function(newCategory) {
-  return this.create(newCategory);
-};
-
-categorySchema.statics.getCategory = function(id) {
-  return this.findById(id);
-};
-
-categorySchema.statics.updateCategory = function(id, newName) {
-  return this.findByIdAndUpdate(id, { categoryName: newName }, { new: true });
-};
-
-categorySchema.statics.deleteCategory = function(id) {
-  return this.findByIdAndDelete(id);
-};
-
 const Category = mongoose.model('Category', categorySchema);
+
+Category.getAll = function() {
+  return Category.find();
+};
+
+Category.addCategory = function(newCategory) {
+  return Category.create(newCategory);
+};
+
+Category.updateCategory = function(id, newName) {
+  return Category.findByIdAndUpdate(id, { categoryName: newName }, { new: true });
+};
+
+Category.deleteCategory = function(id) {
+  return Category.findByIdAndDelete(id);
+};
+
 module.exports = Category;
