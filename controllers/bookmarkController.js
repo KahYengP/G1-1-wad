c1239ebd13ed990c5afb6baa983f8ef450c92fe2
KahYengP <sharonphang05@gmail.com>
2026-03-30 14:40:37 +0800

    Fix bookmark

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,120 +1,128 @@
-const Bookmark = require("../models/Bookmark")
-const Recipe = require("../models/Recipe")
-const Category = require('../models/Category');
+const Bookmark = require("../models/Bookmark");
+const Recipe = require("../models/Recipe");
+const Category = require("../models/Category");
 
-exports.createBookmark = async(req,res) => {
-    try {
-        const userId=req.session.userId
-        const recipeId=req.body.recipeId
-        const note=req.body.note
-        const category=req.body.category
+exports.createBookmark = async (req, res) => {
+  try {
+    const userId = req.session.userId;
+    const recipeId = req.body.recipeId;
 
-        if (!userId) {
-            return res.redirect("/login")
-        }
-        const existingRecipe = await Recipe.findById(recipeId)
-        if (!existingRecipe) {
-            return res.send('Recipe not found.')
-        }
-        const existing = await Bookmark.findOne({userId:userId, recipeId: recipeId}).populate('recipeId').populate('category')
-        if (existing) {
-            return res.send('Recipe already bookmarked.')
-        }
-
-        const bookmark = new Bookmark({
-            userId: userId, 
-            recipeId: recipeId,
-            note: note || "",
-            category: existingRecipe.category._id
-        })
-        await bookmark.save()
-        res.redirect('/bookmarks')
-    } catch(error) {
-        console.error(error)
-        res.send(error.message)
+    if (!userId) {
+      return res.redirect("/login");
     }
-}
 
-exports.readBookmarks = async(req,res) => {
-    try {
-        const userId=req.session.userId
-        if (!userId) {
-            return res.redirect('/login')
-        }
-
-        const bookmarks = await Bookmark.find({userId: userId}).populate('recipeId').populate('category')
-        res.render("bookmarks", {bookmarks: bookmarks})
-    } catch(error) {
-        console.error(error)
-        res.send("Error loading bookmarks.")
+    const existingRecipe = await Recipe.findById(recipeId);
+    if (!existingRecipe) {
+      return res.send("Recipe not found.");
     }
-}
 
-exports.updateBookmark = async(req,res) => {
-    try { 
-        const userId=req.session.userId
-        const bookmarkId=req.body.bookmarkId
-        const note=req.body.note
-        const category=req.body.category
-
-        if (!userId) {
-            return res.redirect('/login')
-        }
-
-        const updatedBookmark = await Bookmark.findOneAndUpdate( 
-            {_id: bookmarkId, userId: userId}, 
-            {note: note, category: category},
-            {new: true}
-        )
-        if (!updatedBookmark) {
-            return res.send('Bookmark not found.')
-        }
-        res.redirect('/bookmarks')
-    } catch(error) {
-        console.error(error)
-        res.send("Error updating bookmark." + error.message)
+    const existing = await Bookmark.findOne({
+      userId: userId,
+      recipeId: recipeId,
+    });
+    if (existing) {
+      return res.send("Recipe already bookmarked.");
     }
-}
 
-exports.deleteBookmark = async(req,res) => {
-    try {
-        const userId=req.session.userId
-        const bookmarkId=req.body.bookmarkId
+    const bookmark = new Bookmark({
+      userId: userId,
+      recipeId: recipeId,
+      note: "",
+      category: existingRecipe.category ? existingRecipe.category : null,
+    });
+    await bookmark.save();
+    res.redirect("/bookmarks");
+  } catch (error) {
+    console.error(error);
+    res.send(error.message);
+  }
+};
 
-        if (!userId) {
-            return res.redirect('/login')
-        }
-        const deletedBookmark = await Bookmark.findOneAndDelete( 
-            {_id: bookmarkId, userId: userId}
-        )
-        if (!deletedBookmark) {
-            return res.send("Bookmark not found.")
-        }
-        res.redirect("/bookmarks")
-    } catch(error) {
-        console.error(error)
-        res.send('Error deleting bookmark.')
+exports.readBookmarks = async (req, res) => {
+  try {
+    const userId = req.session.userId;
+    if (!userId) {
+      return res.redirect("/login");
     }
-}
 
-exports.showEditBookmarkForm = async(req,res) => {
-    try {
-        const userId=req.session.userId
-        const bookmarkId=req.body.bookmarkId
+    const bookmarks = await Bookmark.find({ userId: userId })
+      .populate("recipeId")
+      .populate("category");
+    res.render("bookmarks", { bookmarks: bookmarks });
+  } catch (error) {
+    console.error(error);
+    res.send("Error loading bookmarks.");
+  }
+};
 
-        const CategoryList = await Category.find()
+exports.updateBookmark = async (req, res) => {
+  try {
+    const userId = req.session.userId;
+    const bookmarkId = req.body.bookmarkId;
+    const note = req.body.note;
+    const category = req.body.category;
 
-        if (!userId) {
-            return res.redirect("/login")
-        }
-        
-        const bookmark= await Bookmark.findOne({_id: bookmarkId, userId: userId}).populate('recipeId').populate('category')
-        if (!bookmark) {
-            return res.send('Bookmark not found.')
-        }
-        res.render('edit-bookmark', {bookmark, CategoryList})
-    } catch(error) {
-        console.error(error)
-        res.send('Error loading the edit page.')
+    if (!userId) {
+      return res.redirect("/login");
     }
-} 
+
+    const updatedBookmark = await Bookmark.findOneAndUpdate(
+      { _id: bookmarkId, userId: userId },
+      { note: note, category: category },
+      { new: true },
+    );
+    if (!updatedBookmark) {
+      return res.send("Bookmark not found.");
+    }
+    res.redirect("/bookmarks");
+  } catch (error) {
+    console.error(error);
+    res.send("Error updating bookmark." + error.message);
+  }
+};
+
+exports.deleteBookmark = async (req, res) => {
+  try {
+    const userId = req.session.userId;
+    const bookmarkId = req.body.bookmarkId;
+
+    if (!userId) {
+      return res.redirect("/login");
+    }
+    const deletedBookmark = await Bookmark.findOneAndDelete({
+      _id: bookmarkId,
+      userId: userId,
+    });
+    if (!deletedBookmark) {
+      return res.send("Bookmark not found.");
+    }
+    res.redirect("/bookmarks");
+  } catch (error) {
+    console.error(error);
+    res.send("Error deleting bookmark.");
+  }
+};
+
+exports.showEditBookmarkForm = async (req, res) => {
+  try {
+    const userId = req.session.userId;
+    const bookmarkId = req.body.bookmarkId;
+
+    const CategoryList = await Category.find();
+
+    if (!userId) {
+      return res.redirect("/login");
+    }
+
+    const bookmark = await Bookmark.findOne({ _id: bookmarkId, userId: userId })
+      .populate("recipeId")
+      .populate("category");
+    if (!bookmark) {
+      return res.send("Bookmark not found.");
+    }
+    res.render("edit-bookmark", { bookmark, CategoryList });
+  } catch (error) {
+    console.error(error);
+    res.send("Error loading the edit page.");
+  }
+};
--- models/Bookmark.js
+++ models/Bookmark.js
@@ -1,30 +1,29 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
 const bookmarkSchema = new mongoose.Schema({
-    userId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required: true
-    },
-    recipeId:{
-        type: mongoose.Schema.Types.ObjectId, 
-        ref:'Recipe',
-        required: true 
-    }, 
-    category:{
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Category',
-        required: true
-    }, 
-    note:{
-        type: String, 
-        default:""
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now
-    }   
-})
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  recipeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Recipe",
+    required: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: false,
+  },
+  note: {
+    type: String,
+    default: "",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports = mongoose.model("Bookmark", bookmarkSchema)
-
+module.exports = mongoose.model("Bookmark", bookmarkSchema);
